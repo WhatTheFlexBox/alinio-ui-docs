82f2572c73da11b5d0071c5fe4a8284dccb9592e
Rich Turner <rich-turner@hotmail.com>
2023-11-18 21:08:18 +0000

    logo colour

--- src/assets/media/custom-icons/AlinioLogo.tsx
+++ src/assets/media/custom-icons/AlinioLogo.tsx
@@ -7,7 +7,7 @@ const AlinioLogo = (props: SVGProps<SVGSVGElement>) => (
 		viewBox='0 0 597.4 598.7'
 		className='logo'>
 		<path
-			fill='#121F34'
+			fill='#1c1c1c'
 			d='M357.5 598.7v-1.9c0-132.1 107.1-239.2 239.2-239.2h.7V0h-.7C267.2 0 0 267.2 0 596.8v1.9h357.5z'
 		/>
 	</svg>
